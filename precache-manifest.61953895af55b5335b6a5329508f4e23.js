dd0143421f0db9d71888bae3e4981c6ecc523c66
Seibeaaar <yuribar4520@gmail.com>
2020-05-28 17:39:31 +0300

    Updates

---
+++ precache-manifest.61953895af55b5335b6a5329508f4e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929b144061f7850f338a9059ebf00e42",
+    "url": "/Component-generator/index.html"
+  },
+  {
+    "revision": "45d2744f982fdea0bf08",
+    "url": "/Component-generator/static/css/main.002e8028.chunk.css"
+  },
+  {
+    "revision": "6e0ac029de8e44d647c5",
+    "url": "/Component-generator/static/js/2.b210dcf5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Component-generator/static/js/2.b210dcf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d2744f982fdea0bf08",
+    "url": "/Component-generator/static/js/main.fe3141f2.chunk.js"
+  },
+  {
+    "revision": "c49c12140b01badc1887",
+    "url": "/Component-generator/static/js/runtime-main.dd87620f.js"
+  }
+]);
